5934f124ac346dd629af62f58830da854411287c
SBola97 <sebassenk1997@gmail.com>
2022-08-05 18:25:03 -0500

    modificaciones en el footer y eliminacion de section contacto

--- footer.php
+++ footer.php
@@ -1,21 +1,33 @@
 <footer>
-        <div id="social-networks">
-                <a href="https://www.facebook.com/facultadcienciasespochoficial" target="_blank">
-                        <img class="btn." src="<?php echo get_template_directory_uri(); ?>/assets/icons/facebook_logo.svg" alt="logo de facebook">
-                </a>
-                <a href="https://twitter.com/fcienciasespoch" target="_blank">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/twitter_logo.svg" alt="logo de facebook">
-                </a>
-                
-        </div>
-        <h2>SÍGUENOS EN REDES SOCIALES</h2>
         <div class="footer-contenido">
-                <p>Ingeniería Ambiental - Facultad de Ciencias - ESPOCH</p>
-                <p>Términos de uso | Políticas de privacidad | Acerca de</p>
-        </div>
-        <div>
-                &copy;Derechos Reservados 2022
-                <p>Autores: Sebastian Bolaños & Ayrton Avalos ♥️</p>
+                <div id="social-networks">
+                        <h2>Síguenos en Redes Sociales</h2>  
+                        <a href="https://www.facebook.com/facultadcienciasespochoficial" target="_blank">
+                                <img class="btn." src="<?php echo get_template_directory_uri(); ?>/assets/icons/facebook_logo.svg" alt="logo de facebook">
+                        </a>
+                        <a href="https://twitter.com/fcienciasespoch" target="_blank">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/twitter_logo.svg" alt="logo de facebook">
+                        </a>
+                        <a href="" target="_blank">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/gmail-icon.svg" alt="logo de gmail">
+                        </a>
+                </div>
+                <div class= "contacto">
+                        <h2>Contacto</h2>
+                        <p>0986825637</p>
+                        <p>Ing.Andres Beltrán - Coordinador de Carrera</p>
+                        <a href="" target="_blank">abeltran@espoch.edu.ec</a>
+                </div>
+                <div class= "ubicacion">
+                        <h2>Ubicación</h2>
+                        <p>Panamericana Sur km 1 1/2, Riobamba-Ecuador</p>
+                        <a href="" target="_blank">Ubicación en Google Maps</a> 
+                </div>
+                <div class="contenido">
+                        <p>Ingeniería Ambiental - Facultad de Ciencias - ESPOCH</p>
+                        &copy;Derechos Reservados 2022
+                        <p>Autores: Sebastian Bolaños & Ayrton Avalos ♥️</p>
+                </div>
         </div>
         
 </footer>
