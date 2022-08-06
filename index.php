0ba9ab24d9ab22557774209e1ba061754ef119b2
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-08-06 18:30:43 -0500

    proyecto completo

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
-<?php 
-$site = get_the_title();
-get_header(); 
-?>
-<main class="page-content-news">
-    <section id="news">
-        <?php 
-            if(have_posts()){
-                while(have_posts()) {
-                    the_post();
-                    get_template_part('template-parts/content','archive');
-                } 
-            }
-        ?>
-    </section>
-</main>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<?php get_footer(); ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
--- template-parts/content-article.php
+++
@@ -1,5 +0,0 @@
-<main class="page-content">
-    <?php 
-        the_content();
-    ?>
-</main>
