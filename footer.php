c41de611294509d473e58059c2dfaa4ed40883c0
SBola97 <sebassenk1997@gmail.com>
2022-08-06 12:08:24 -0500

    cambios en el footer respecto a distribución de contenido

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-        <div class="footer-contenido">
+        <div id="container-footer">
                 <div id="social-networks">
                         <h2>Síguenos en Redes Sociales</h2>  
                         <a href="https://www.facebook.com/facultadcienciasespochoficial" target="_blank">
@@ -8,26 +8,34 @@
                         <a href="https://twitter.com/fcienciasespoch" target="_blank">
                                 <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/twitter_logo.svg" alt="logo de facebook">
                         </a>
-                        <a href="" target="_blank">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/gmail-icon.svg" alt="logo de gmail">
-                        </a>
                 </div>
-                <div class= "contacto">
+                <div id= "contacto">
                         <h2>Contacto</h2>
-                        <p>0986825637</p>
-                        <p>Ing.Andres Beltrán - Coordinador de Carrera</p>
-                        <a href="" target="_blank">abeltran@espoch.edu.ec</a>
+                        <div class="footer-inf">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/telephone-icon.svg" alt="logo de telefono">
+                                <p>0986825637</p>
+                        </div>
+                        <div class="footer-inf">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/boss-icon.svg" alt="logo de coordinador">
+                                <p>Ing.Andres Beltrán - Coordinador de Carrera</p>
+                        </div>
+                        <div class="footer-inf">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/gmail-icon.svg" alt="logo de gmail">
+                        <a href="abeltran@espoch.edu.ec" target="_blank">abeltran@espoch.edu.ec</a>
+                        </div>
+                        
                 </div>
-                <div class= "ubicacion">
+                <div id= "ubicacion">
                         <h2>Ubicación</h2>
                         <p>Panamericana Sur km 1 1/2, Riobamba-Ecuador</p>
+                        <p>Edificio de Facultad de Ciencias, 2do. Piso</p>
                         <a href="" target="_blank">Ubicación en Google Maps</a> 
                 </div>
-                <div class="contenido">
-                        <p>Ingeniería Ambiental - Facultad de Ciencias - ESPOCH</p>
-                        &copy;Derechos Reservados 2022
-                        <p>Autores: Sebastian Bolaños & Ayrton Avalos ♥️</p>
-                </div>
+        </div>
+        <div id="contenido">
+                <p>Ingeniería Ambiental - Facultad de Ciencias - ESPOCH
+                &copy;Derechos Reservados 2022</p>
+                <p>Autores: Sebastian Bolaños & Ayrton Avalos ♥️</p>
         </div>
         
 </footer>
